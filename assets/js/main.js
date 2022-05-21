f9bf59b7f092592a499d39d74f1000d4a9505d8f
aledark504 <alejandrorgp18@gmail.com>
2022-05-21 17:11:05 -0400

    index-ready

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    let scrollNow = window.pageYOffset;
+    $(window).on('scroll', function () {
+        let scroll = $(window).scrollTop();
+        if (scroll > 400) {
+            $(".topheader").addClass("topheader-with-background");
+        } else {
+            $(".topheader").removeClass("topheader-with-background");
+        }
+        if(scrollNow > scroll) {
+            $(".topheader").css("transform", "translateY(0)");
+        }else{
+            $(".topheader").css("transform", "translateY(-100%)");
+        }
+        scrollNow = scroll;
+    });
+
+    var containerLoader = $('.container-loader');
+    console.log(containerLoader);
+    var loader = $('.loader');
+   
+    $(window).on("pageshow", function () {
+        if (loader) {
+            $('.container-loader').fadeOut(300);
+        }
+        document.body.style.overflowY = 'visible';
+    });
+});
